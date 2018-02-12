0720b8657250d28390da11c092b8676950484c00
Polina Lappo <polina.lappo@soft-werke.com>
2018-02-12 08:44:12 +0300

    basic rest api for checklist

---
+++ src/main/java/com/softwerke/tms/controller/ChecklistController.java
@@ -0,0 +1,23 @@
+package com.softwerke.tms.controller;
+
+import com.softwerke.tms.model.Checklist;
+import com.softwerke.tms.service.ChecklistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ChecklistController {
+
+    @Autowired
+    ChecklistService checklistService;
+
+    @RequestMapping("/checklists")
+    public List<Checklist> getAllChecklists (@RequestParam(value="name", required=false, defaultValue="World") String name) {
+        List<Checklist> checklists = checklistService.getChecklists();
+        return checklists;
+    }
+}
