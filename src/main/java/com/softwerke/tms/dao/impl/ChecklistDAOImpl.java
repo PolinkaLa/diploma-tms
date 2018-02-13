8643f787ef9dc5e2cba803c791759d3e714fd992
Polina Lappo <polina.lappo@soft-werke.com>
2018-02-13 23:30:04 +0300

    basic rest api for checklist

--- src/main/java/com/softwerke/tms/dao/impl/ChecklistDAOImpl.java
+++ src/main/java/com/softwerke/tms/dao/impl/ChecklistDAOImpl.java
@@ -2,16 +2,24 @@ package com.softwerke.tms.dao.impl;
 
 import com.softwerke.tms.dao.ChecklistDAO;
 import com.softwerke.tms.model.Checklist;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 @Service
 public class ChecklistDAOImpl extends JdbcDaoSupport implements ChecklistDAO {
+
+    @Autowired
+    ChecklistDAOImpl (DataSource dataSource) {
+        setDataSource(dataSource);
+    }
+
     @Override
     public void insertChecklist(Checklist checklist) {
 
