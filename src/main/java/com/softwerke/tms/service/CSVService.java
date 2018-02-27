81ded7ce21500d27f28a8460fdbc9a7da5a6b0c2
Polina Lappo <polina.lappo@soft-werke.com>
2018-02-28 00:56:38 +0300

    add csv service

---
+++ src/main/java/com/softwerke/tms/service/CSVService.java
@@ -0,0 +1,8 @@
+package com.softwerke.tms.service;
+
+public interface CSVService {
+
+    void importChecklist(int checklistID);
+    void exportChecklist(int checklistID);
+
+}
---
+++ src/main/java/com/softwerke/tms/service/impl/CSVServiceImpl.java
@@ -0,0 +1,16 @@
+package com.softwerke.tms.service.impl;
+
+import com.softwerke.tms.service.CSVService;
+
+public class CSVServiceImpl implements CSVService {
+
+    @Override
+    public void importChecklist(int checklistID) {
+
+    }
+
+    @Override
+    public void exportChecklist(int checklistID) {
+
+    }
+}
