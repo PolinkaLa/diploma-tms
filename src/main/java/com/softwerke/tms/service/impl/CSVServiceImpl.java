7873f3a38605384b487d0e7015e4f295f1dece3a
Polina Lappo <polina.lappo@soft-werke.com>
2018-03-29 01:10:37 +0300

    delete backend export

--- src/main/java/com/softwerke/tms/service/impl/CSVServiceImpl.java
+++ src/main/java/com/softwerke/tms/service/impl/CSVServiceImpl.java
@@ -46,44 +46,4 @@ public class CSVServiceImpl implements CSVService {
         reader.close();
         return emps;
     }
-
-    @Override
-    public void exportChecklist(int checklistID) throws IOException {
-
-        List<Test> test = testDAO.getTests();
-        String COMMA_DELIMITER = "; ";
-        String LINE_SEPARATOR = "\n";
-
-        String HEADER = "ID; Title; Description; Type; Level";
-
-        FileWriter fileWriter = null;
-
-        try {
-            fileWriter = new FileWriter("D:/test.csv");
-//            fileWriter.append(HEADER);
-//            fileWriter.append(LINE_SEPARATOR);
-            Iterator it = test.iterator();
-            while (it.hasNext()) {
-                Test e = (Test) it.next();
-                fileWriter.append(String.valueOf(e.getId()));
-                fileWriter.append(COMMA_DELIMITER);
-                fileWriter.append(e.getTitle());
-                fileWriter.append(COMMA_DELIMITER);
-                fileWriter.append(e.getDescription());
-                fileWriter.append(COMMA_DELIMITER);
-                fileWriter.append(typeDAO.getTypeName(e.getFkTypeId()));
-                fileWriter.append(COMMA_DELIMITER);
-                fileWriter.append(levelDAO.getLevelName(e.getFkLevelId()));
-                fileWriter.append(LINE_SEPARATOR);
-            }
-        } catch (Exception ee) {
-            ee.printStackTrace();
-        } finally {
-            try {
-                fileWriter.close();
-            } catch (IOException ie) {
-                ie.printStackTrace();
-            }
-        }
-    }
 }
